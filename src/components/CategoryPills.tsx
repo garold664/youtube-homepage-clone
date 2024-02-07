aada53abbbbeae73c82ba84e91f4c667950e7874
garold664 <garold664@gmail.com>
2024-02-07 13:02:53 +0300

    implemented categories scroll via buttons

--- src/components/CategoryPills.tsx
+++ src/components/CategoryPills.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useRef, useState, useEffect, ElementRef } from 'react';
 import Button from './Button';
+import { ChevronLeft, ChevronRight } from 'lucide-react';
 
 type CategoryPillProps = {
   categories: string[];
@@ -7,14 +8,44 @@ type CategoryPillProps = {
   onSelect: (category: string) => void;
 };
 
+const TRANSLATE_AMOUNT = 200;
+
 export default function CategoryPills({
   categories,
   selectedCategory,
   onSelect,
 }: CategoryPillProps) {
+  const [isLeftVisible, setIsLeftVisible] = useState(true);
+  const [isRightVisible, setIsRightVisible] = useState(true);
+  const [translate, setTranslate] = useState(0);
+  // const containerRef = useRef<HTMLDivElement>(null);
+  const containerRef = useRef<ElementRef<'div'>>(null);
+  console.log(translate);
+
+  useEffect(() => {
+    if (containerRef.current == null) return;
+    const edge = containerRef.current.scrollWidth;
+    const width = containerRef.current.clientWidth;
+    // const newTranslate = translate + TRANSLATE_AMOUNT;
+    console.log(edge, width);
+    if (translate + width >= edge) {
+      setIsRightVisible(false);
+    } else {
+      setIsRightVisible(true);
+    }
+    if (translate <= 0) {
+      setIsLeftVisible(false);
+    } else {
+      setIsLeftVisible(true);
+    }
+  }, [categories, translate]);
+
   return (
-    <div className="oveflow-x-hidden relative">
-      <div className="flex whitespace-nowrap gap-3 transition-transform w-[max-content]">
+    <div className="overflow-x-hidden relative" ref={containerRef}>
+      <div
+        className="flex whitespace-nowrap gap-3 transition-transform w-[max-content]"
+        style={{ transform: `translateX(-${translate}px)` }}
+      >
         {categories.map((category) => (
           <Button
             onClick={() => onSelect(category)}
@@ -26,6 +57,52 @@ export default function CategoryPills({
           </Button>
         ))}
       </div>
+
+      {isLeftVisible && (
+        <div
+          className="absolute left-0 top-1/2 -translate-y-1/2
+      bg-gradient-to-r from-white from-50% to-transparent w-24 h-full"
+        >
+          <Button
+            onClick={() =>
+              setTranslate((translate) => {
+                const newTranslate = translate - TRANSLATE_AMOUNT;
+                if (newTranslate <= 0) return 0;
+                return newTranslate;
+              })
+            }
+            variant="ghost"
+            size="icon"
+            className="h-full aspect-square w-auto p-1.5"
+          >
+            <ChevronLeft />
+          </Button>
+        </div>
+      )}
+      {isRightVisible && (
+        <div
+          className="absolute right-0 top-1/2 -translate-y-1/2
+      bg-gradient-to-l from-white from-50% to-transparent w-24 h-full"
+        >
+          <Button
+            variant="ghost"
+            size="icon"
+            className="h-full aspect-square w-auto ml-auto p-1.5"
+            onClick={() => {
+              setTranslate((translate) => {
+                if (containerRef.current == null) return translate;
+                const edge = containerRef.current.scrollWidth;
+                const width = containerRef.current.clientWidth;
+                const newTranslate = translate + TRANSLATE_AMOUNT;
+                if (newTranslate + width >= edge) return edge - width;
+                return newTranslate;
+              });
+            }}
+          >
+            <ChevronRight />
+          </Button>
+        </div>
+      )}
     </div>
   );
 }
